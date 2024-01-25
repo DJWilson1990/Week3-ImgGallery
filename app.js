5af0a5c5deac34b974e1ebef746c2ff425d0273f
DANIEL WILSON <daniel.j.wilson@btinternet.com>
2024-01-25 16:38:05 +0000

    set up selector for thumbnails with styling

--- app.js
+++ app.js
@@ -3,11 +3,28 @@ console.log("Hello");
 const form = document.getElementById("imgSearchForm");
 const thumbnailContainer = document.getElementById("thumbnail-container");
 const mainImage = document.getElementById("main-container");
-let currentlySelectedThumbnail = document.getElementsByClassName("thumbnail", [
-  0 - 9,
-]); //trying to select chosen thumbnail to give it a highlight
+let currentlySelectedThumbnail = 4; //setting the currently selected thumbnail to be the 5th image as default
 
-function selectImage(currentlySelectedThumbnail) {}
+//eventlistener on previous and next button
+document.getElementById("nextBtn").addEventListener("click", next);
+document.getElementById("previousBtn").addEventListener("click", previous);
+
+//setting function for next button
+function next() {
+  currentlySelectedThumbnail = currentlySelectedThumbnail + 1;
+  if (currentlySelectedThumbnail > 9) {
+    currentlySelectedThumbnail = 0; //telling selected to go back to first thumbnail when it reaches the last
+  }
+  updateThumbnailSelector();
+}
+
+function previous() {
+  currentlySelectedThumbnail = currentlySelectedThumbnail - 1;
+  if (currentlySelectedThumbnail < 0) {
+    currentlySelectedThumbnail = 9; //telling selected to go back to last thumbnail when reaches first
+  }
+  updateThumbnailSelector();
+}
 
 form.addEventListener("submit", function (event) {
   event.preventDefault();
@@ -15,7 +32,15 @@ form.addEventListener("submit", function (event) {
   search(query);
 });
 
+//Clearing the images from DOM when new search is entered so we dont end up with 100s of images
+function clearThumbnails() {
+  while (thumbnailContainer.hasChildNodes("div")) {
+    thumbnailContainer.removeChild(thumbnailContainer.lastChild);
+  }
+}
+
 async function search(queryParam) {
+  clearThumbnails();
   let response = await fetch(
     `https://api.unsplash.com/search/photos?page=1&query=${queryParam}&client_id=ib4uIfBhjX9GrxzV6cztReU-W-_T6UMr5JRI5abvXjA`
   );
@@ -25,11 +50,26 @@ async function search(queryParam) {
   for (let i = 0; i < 10; i++) {
     let div = document.createElement("div");
     div.className = "thumbnail";
-    //if i = currentlySelected(need to define), apply an extra class to that element
+    if (i === currentlySelectedThumbnail) {
+      div.classList.add("selected");
+    }
     let img = document.createElement("img");
     img.src = data.results[i].urls.thumb;
     div.appendChild(img);
     thumbnailContainer.appendChild(div);
   }
-  // mainImage.style.backgroundImage = "url(${data.results[4].urls.full})";
+}
+
+//function to know what thumbnail is selected or not. Styled so border is on selected image and the non selected images have opacity added.
+function updateThumbnailSelector() {
+  const thumbnailImages =
+    thumbnailContainer.getElementsByClassName("thumbnail");
+  for (let i = 0; i < 10; i++) {
+    if (i === currentlySelectedThumbnail) {
+      thumbnailImages[i].classList.add("selected");
+    } else {
+      thumbnailImages[i].classList.remove("selected");
+    }
+  }
+  console.log(thumbnailImages);
 }
